4bb52a7d33632356591b09c7a9a1316d5d70d9b9
温 穎峰 <renowan@yahoo.co.jp>
2017-05-07 02:18:10 +0900

    課題：スロット

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Slot from '@/page/slot/TopView'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/slot',
+      name: 'slot',
+      component: Slot
     }
   ]
 })
